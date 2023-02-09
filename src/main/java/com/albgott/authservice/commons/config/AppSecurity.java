bb8bfaec195f343c1435531ef1872cc376525fd9
Alberto Morán <a.moran.cuenca@gmail.com>
2023-02-09 13:42:06 +0100

    change security bean

--- src/main/java/com/albgott/authservice/commons/config/AppSecurity.java
+++ src/main/java/com/albgott/authservice/commons/config/AppSecurity.java
@@ -10,9 +10,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class AppSecurity extends AbstractHttpConfigurer<AppSecurity, HttpSecurity> {
+public class AppSecurity {
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
 
@@ -28,10 +29,11 @@ public class AppSecurity extends AbstractHttpConfigurer<AppSecurity, HttpSecurit
         return new BCryptPasswordEncoder(11);
     }
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable();
         http.authorizeHttpRequests().anyRequest().permitAll();
+        return http.build();
     }
 
 }
